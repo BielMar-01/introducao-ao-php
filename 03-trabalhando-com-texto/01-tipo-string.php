a3db4e712a381969d68b306ae248b4b4827d0688
Gabriel Marques <bielmarques922@gmail.com>
2022-04-29 09:54:53 -0300

    Trabalhando com texto - Tipo String

---
+++ 03-trabalhando-com-texto/01-tipo-string.php
@@ -0,0 +1,10 @@
+<?php
+
+// Com aspas duplas é possivel colocar variveis dentro de strings concatenando tudo
+
+$idade = 20;
+$nome = 'Gabriel';
+
+//echo 'Olá mundo! Eu tenho ' . $idade . ' anos.';
+
+echo "Olá mundo! Meu nome é $nome e tenho $idade anos.";
