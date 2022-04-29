81f1dbf02466941a3d7464c704f7767eb78c2bb3
Gabriel Marques <bielmarques922@gmail.com>
2022-04-29 18:42:35 -0300

    Trabalhando com texto - Caracteres especiais

---
+++ 03-trabalhando-com-texto/02-caracteres-especiais.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 20;
+$nome = 'Gabriel';
+
+echo "Olá mundo!\n";
+//echo "Olá mundo!" . PHP_EOL; Esse comando tem a função de quebra de linha assim  como o \n
+// echo "\n"; Essa quebra linha
+// echo "\t"; Ess da um Tab
+echo "Meu nome é $nome e tenho $idade anos.";
