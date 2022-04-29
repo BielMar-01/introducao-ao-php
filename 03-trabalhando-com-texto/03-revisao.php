2f64a8bcc218b735b5543b53a4a7c106b3765ef5
Gabriel Marques <bielmarques922@gmail.com>
2022-04-29 18:49:55 -0300

    Trabalhando com texto - Revisão até aqui

---
+++ 03-trabalhando-com-texto/03-revisao.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 20;
+$nome = 'Gabriel';
+
+echo "Olá mundo!\n";
+echo "Meu nome é \"$nome\" e tenho $idade anos."; // \"$var\" deixa a variavel com aspas
+
+echo PHP_EOL . PHP_EOL;
+
+echo "Você só pode entrar, se tiver mais de 18 anos\n";
+echo "Você tem $idade anos. Pode entrar";
