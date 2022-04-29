6192a0661bdf97511aac4004549ee1b4da8fa7ce
Gabriel Marques <bielmarques922@gmail.com>
2022-04-28 21:35:21 -0300

    Variáveis e tipos - Operações matemáticas

---
+++ 02-variaveis-e-tipos/02-operacoes-matematicas.php
@@ -0,0 +1,15 @@
+<?php
+$idade = 20;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+// echo $idadeDaqui10Anos;
+echo $idadeHa5Anos;
+
+// Tipos de operações matematicas
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doisAoCubo = 2 ** 3; // O ** duas vezes se refere a operações exponenciais 
+$restoDaDivisao = 10 % 3;
